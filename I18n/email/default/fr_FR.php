fac4dec7028edb9cd00077707ad69849c57d9892
Chevrier <anthony@meedle.fr>
2015-09-29 10:24:35 +0200

    traduction
    une phrase était restée en anglais.

--- I18n/email/default/fr_FR.php
+++ I18n/email/default/fr_FR.php
@@ -2,7 +2,7 @@
 
 return array(
     'Best Regards.' => 'Cordialement.',
-    'Feel free to contact us for any further information' => 'Feel free to contact us for any further information',
+    'Feel free to contact us for any further information' => 'N\'hésitez pas à nous contacter pour toute information supplémentaire',
     'Hi' => 'Bonjour',
     'Some product(s) is (are) going to be out of stock :' => 'certains produits vont bientôt être indisponible :',
     'The product %title (%pse) is available again.' => 'Le produit %title (%pse) est à nouveau disponible.',
